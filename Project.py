92965cbdce07b2c3cdf92edb6d6ac85eaaf29b9d
VaryaEliseykina <varyaeli@yandex.ru>
2020-02-04 19:09:11 +0300

    Экран заставки

--- Project.py
+++ Project.py
@@ -1,13 +1,14 @@
 import pygame
 import sys
 import os
+import random
 
 pygame.init()
 pygame.key.set_repeat(200, 70)
 
 FPS = 50
-WIDTH = 500
-HEIGHT = 500
+WIDTH = 600
+HEIGHT = 400
 STEP = 10
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -36,18 +37,18 @@ def load_image(name, color_key=None):
 
 
 def start_screen():
-    intro_text = ["ШАРИКИ"]
+    intro_text = ["ATARI BREAKOUT", "PRESS SPACE TO START"]
 
     fon = pygame.transform.scale(load_image('fon.png'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
-    text_coord = 100
+    text_coord = 250
     for line in intro_text:
         string_rendered = font.render(line, 1, pygame.Color('black'))
         intro_rect = string_rendered.get_rect()
         text_coord += 10
         intro_rect.top = text_coord
-        intro_rect.x = 10
+        intro_rect.x = 150
         text_coord += intro_rect.height
         screen.blit(string_rendered, intro_rect)
 
@@ -56,9 +57,38 @@ def start_screen():
             if event.type == pygame.QUIT:
                 terminate()
             elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
+                play_screen()  # начинаем игру
         pygame.display.flip()
         clock.tick(FPS)
+
+
+def play_screen():
+    pass
+
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, colour, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 20)).convert()
+        self.image.fill(colour)
+        self.rect = self.image.get_rect()
+        # set the position of the block
+        self.rect.left = x
+        self.rect.top = y
+
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, radius, x, y):
+        super().__init__()
+        self.radius = radius
+        self.image = pygame.Surface((2 * radius, 2 * radius), pygame.SRCALPHA, 32)
+        pygame.draw.circle(self.image, pygame.Color("red"), (radius, radius), radius)
+        self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
+        self.vx = random.randint(-5, 5)
+        self.vy = random.randrange(-5, 5)
+
+    def update(self):
+        self.rect = self.rect.move(self.vx, self.vy)
 
 
 start_screen()

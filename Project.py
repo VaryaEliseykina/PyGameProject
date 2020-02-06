450506f6298050dcbae8b7f145826319acaa593a
VaryaEliseykina <varyaeli@yandex.ru>
2020-02-06 13:41:40 +0300

    Добавлен класс шарика, экраны победы и поражения

--- Project.py
+++ Project.py
@@ -6,14 +6,20 @@ import random
 pygame.init()
 pygame.key.set_repeat(200, 70)
 
-FPS = 50
+FPS = 30
 WIDTH = 600
 HEIGHT = 400
-STEP = 10
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 background = pygame.Surface(screen.get_size()).convert()
 clock = pygame.time.Clock()
+
+score = 0
+brick_group_red = pygame.sprite.Group()
+brick_group_yellow = pygame.sprite.Group()
+brick_group_green = pygame.sprite.Group()
+brick_group_blue = pygame.sprite.Group()
+rect_group = []
 
 
 def terminate():
@@ -66,45 +72,148 @@ def start_screen():
 
 #  Экран игры
 def play_screen():
+    ball = Ball()
+    ball_group = pygame.sprite.Group(ball)
+
     background.fill((0, 0, 0))
     screen.blit(background, (0, 0))
-    build_bricks()
+
+    score = 0
+    x_ = 300
+    x_change = 0
+    is_defeated = False
+    right = True
+    left = True
+
+    brick_group_red.add(Wall((255, 0, 0), 1, 1), Wall((255, 0, 0), 61, 1), Wall((255, 0, 0), 121, 1),
+                        Wall((255, 0, 0), 181, 1), Wall((255, 0, 0), 241, 1), Wall((255, 0, 0), 301, 1),
+                        Wall((255, 0, 0), 361, 1), Wall((255, 0, 0), 421, 1), Wall((255, 0, 0), 481, 1),
+                        Wall((255, 0, 0), 541, 1))
+    brick_group_yellow.add(Wall((255, 255, 0), 31, 40), Wall((255, 255, 0), 91, 40), Wall((255, 255, 0), 151, 40),
+                           Wall((255, 255, 0), 211, 40), Wall((255, 255, 0), 271, 40), Wall((255, 255, 0), 331, 40),
+                           Wall((255, 255, 0), 391, 40), Wall((255, 255, 0), 451, 40), Wall((255, 255, 0), 511, 40))
+    brick_group_green.add(Wall((0, 255, 0), 121, 79), Wall((0, 255, 0), 181, 79), Wall((0, 255, 0), 241, 79),
+                          Wall((0, 255, 0), 301, 79), Wall((0, 255, 0), 361, 79), Wall((0, 255, 0), 421, 79))
+    brick_group_blue.add(Wall((0, 0, 255), 151, 118), Wall((0, 0, 255), 211, 118), Wall((0, 0, 255), 271, 118),
+                         Wall((0, 0, 255), 331, 118), Wall((0, 0, 255), 391, 118))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    if left:
+                        x_change = -10
+                if event.key == pygame.K_RIGHT:
+                    if right:
+                        x_change = 10
+                if event.key == pygame.K_SPACE:
+                    terminate()
+            if event.type == pygame.KEYUP:
+                x_change = 0
+        ball_group.clear(screen, background)
+        x_ += x_change
+        for ball in ball_group:
+            ball.update(True)
+        screen.fill((0, 0, 0))
+        ball_group.draw(screen)
+        Rect = pygame.draw.rect(screen, (255, 255, 255), (x_, 378, 50, 4))
+        rect_group.append(Rect)
+
+        set_bricks(brick_group_red)
+        set_bricks(brick_group_yellow)
+        set_bricks(brick_group_green)
+        set_bricks(brick_group_blue)
+
+        for i in rect_group:
+            if i != Rect:
+                rect_group.remove(i)
+
+        text = pygame.font.SysFont(None, 30).render("Score:" + repr(score), True, (255, 255, 255), (0, 0, 0))
+        textRect = text.get_rect()
+        textRect.bottomright = screen.get_rect().bottomright
+        screen.blit(text, textRect)
+
+        for ball in ball_group:
+            ball.check_defeat()
+
+        for ball in ball_group:
+            ball.check_victory()
+
+        clock.tick(FPS)
+        pygame.display.flip()
 
 
-#  Класс, отвечающий за кирпичики
+def game_over_screen():
+    screen.fill((0, 0, 0))
+    Font = pygame.font.SysFont(None, 30)
+    Font2 = pygame.font.SysFont(None, 72)
+
+    text2 = Font2.render("GAME OVER", True, (255, 255, 255), (0, 0, 0))
+    text3 = Font.render("Press space to restart", True, (255, 255, 255), (0, 0, 0))
+    text7 = Font.render("Score:" + repr(score), True, (255, 255, 255), (0, 0, 0))
+    textRect2 = text2.get_rect()
+    textRect3 = text3.get_rect()
+    textRect7 = text7.get_rect()
+    textRect2.center = screen.get_rect().center
+    textRect3.midbottom = screen.get_rect().midbottom
+    textRect7.midtop = screen.get_rect().midtop
+    screen.blit(text2, textRect2)
+    screen.blit(text3, textRect3)
+    screen.blit(text7, textRect7)
+    is_defeated = True
+    #if musicPlaying:
+        #pygame.mixer.music.stop()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                play_screen()  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def victory_screen():
+    screen.fill((0, 0, 0))
+    Font = pygame.font.SysFont(None, 30)
+    Font2 = pygame.font.SysFont(None, 72)
+
+    text2 = Font2.render("VICTORY", True, (255, 255, 255), (0, 0, 0))
+    text3 = Font.render("Press space to restart", True, (255, 255, 255), (0, 0, 0))
+    text7 = Font.render("Score:" + repr(score), True, (255, 255, 255), (0, 0, 0))
+    textRect2 = text2.get_rect()
+    textRect3 = text3.get_rect()
+    textRect7 = text7.get_rect()
+    textRect2.center = screen.get_rect().center
+    textRect3.midbottom = screen.get_rect().midbottom
+    textRect7.midtop = screen.get_rect().midtop
+    screen.blit(text2, textRect2)
+    screen.blit(text3, textRect3)
+    screen.blit(text7, textRect7)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                play_screen()  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+#  Кирпичики
 class Wall(pygame.sprite.Sprite):
     def __init__(self, colour, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((50, 20)).convert()
         self.image.fill(colour)
         self.rect = self.image.get_rect()
-        # set the position of the block
+        # координаты
         self.rect.left = x
         self.rect.top = y
-
-
-#  Кирпичики
-def build_bricks():
-    brick_group_red = pygame.sprite.Group(Wall((255, 0, 0), 1, 1), Wall((255, 0, 0), 61, 1), Wall((255, 0, 0), 121, 1),
-                                          Wall((255, 0, 0), 181, 1), Wall((255, 0, 0), 241, 1),
-                                          Wall((255, 0, 0), 301, 1), Wall((255, 0, 0), 361, 1),
-                                          Wall((255, 0, 0), 421, 1), Wall((255, 0, 0), 481, 1),
-                                          Wall((255, 0, 0), 541, 1))
-    set_bricks(brick_group_red)
-    brick_group_yellow = pygame.sprite.Group(Wall((255, 255, 0), 31, 40), Wall((255, 255, 0), 91, 40),
-                                             Wall((255, 255, 0), 151, 40), Wall((255, 255, 0), 211, 40),
-                                             Wall((255, 255, 0), 271, 40), Wall((255, 255, 0), 331, 40),
-                                             Wall((255, 255, 0), 391, 40), Wall((255, 255, 0), 451, 40),
-                                             Wall((255, 255, 0), 511, 40))
-    set_bricks(brick_group_yellow)
-    brick_group_green = pygame.sprite.Group(Wall((0, 255, 0), 121, 79), Wall((0, 255, 0), 181, 79),
-                                            Wall((0, 255, 0), 241, 79), Wall((0, 255, 0), 301, 79),
-                                            Wall((0, 255, 0), 361, 79), Wall((0, 255, 0), 421, 79))
-    set_bricks(brick_group_green)
-    brick_group_blue = pygame.sprite.Group(Wall((0, 0, 255), 151, 118), Wall((0, 0, 255), 211, 118),
-                                           Wall((0, 0, 255), 271, 118), Wall((0, 0, 255), 331, 118),
-                                           Wall((0, 0, 255), 391, 118))
-    set_bricks(brick_group_blue)
 
 
 #  Вывод кирпичиков на экран
@@ -115,17 +224,95 @@ def set_bricks(brick_group):
 
 #  Шарик
 class Ball(pygame.sprite.Sprite):
-    def __init__(self, radius, x, y):
+    def __init__(self, radius=5):
         super().__init__()
         self.radius = radius
         self.image = pygame.Surface((2 * radius, 2 * radius), pygame.SRCALPHA, 32)
-        pygame.draw.circle(self.image, pygame.Color("red"), (radius, radius), radius)
-        self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
-        self.vx = random.randint(-5, 5)
-        self.vy = random.randrange(-5, 5)
+        pygame.draw.circle(self.image, (255, 255, 255), (radius, radius), radius)
+        self.rect = self.image.get_rect()
 
-    def update(self):
-        self.rect = self.rect.move(self.vx, self.vy)
+        #  координаты
+        self.rect.left = random.randrange(0, screen.get_width() - self.rect.width)
+        self.rect.top = random.randrange(162, 200)
+        self.dir_x = random.choice([-1, 1])
+        self.dir_y = 1
+        #  скорость
+        self.speed = 3
+
+    #  Сбиваем кирпичики
+    def update(self, corr):
+        global score
+
+        check = self.rect.move(self.speed * self.dir_x, 0)
+        for rect in rect_group:
+            if rect.colliderect(check):
+                self.dir_x *= -1
+                break
+
+        check = self.rect.move(0, self.speed * self.dir_y)
+        for rect in rect_group:
+            if rect.colliderect(check):
+                self.dir_y *= -1
+                break
+
+        #  красные
+        check = self.rect.move(0, self.speed * self.dir_y)
+        for brick in brick_group_red:
+            if brick.rect.colliderect(check):
+                brick_group_red.remove(brick)
+                self.dir_y *= -1
+                self.speed = 6.5
+                score += 1
+                # pickUpSound.play()
+                break
+
+        #  желтые
+        check = self.rect.move(0, self.speed * self.dir_y)
+        for brick in brick_group_yellow:
+            if brick.rect.colliderect(check):
+                brick_group_yellow.remove(brick)
+                self.dir_y *= -1
+                self.speed = 6
+                score += 1
+                # pickUpSound.play()
+                break
+
+        #  зеленые
+        check = self.rect.move(0, self.speed * self.dir_y)
+        for brick in brick_group_green:
+            if brick.rect.colliderect(check):
+                brick_group_green.remove(brick)
+                self.dir_y *= -1
+                self.speed = 4
+                score += 1
+                # pickUpSound.play()
+                break
+
+        #  синие
+        check = self.rect.move(0, self.speed * self.dir_y)
+        for brick in brick_group_blue:
+            if brick.rect.colliderect(check):
+                brick_group_blue.remove(brick)
+                self.dir_y *= -1
+                self.speed = 3
+                score += 1
+                # pickUpSound.play()
+                break
+
+        # Смена направления
+        if self.rect.left < 0 or self.rect.right >= screen.get_width():
+            self.dir_x *= -1
+        if self.rect.top < 0:
+            self.dir_y *= -1
+        self.rect.move_ip(self.speed * self.dir_x, self.speed * self.dir_y)
+
+    def check_defeat(self):
+        if self.rect.bottom >= screen.get_height():
+            game_over_screen()
+
+    def check_victory(self):
+        if len(brick_group_red) == len(brick_group_yellow) == len(brick_group_green) == len(brick_group_blue) == 0:
+            victory_screen()
 
 
 start_screen()
